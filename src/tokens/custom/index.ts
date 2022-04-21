07d83fef032bf5bfbffb6a25379923341b895470
Jigar Patel <jigar@arc10.io>
2022-04-21 13:41:01 -0400

    BNB Testnet

--- src/tokens/custom/index.ts
+++ src/tokens/custom/index.ts
@@ -3,7 +3,7 @@ import {
 } from '../../types';
 
 export const customTokens: NetworkTokens = {
-  canonicalWrappedEther: { name: 'Rinkeby Canonical Wrapped Ether', symbol: 'WETH', decimals: 18, address: '0xc778417e063141139fce010982780140aa0cd5ab' },
+  canonicalWrappedEther: { name: 'Testnet Canonical Wrapped BNB', symbol: 'WETH', decimals: 18, address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd' },
   otherTokens: [
     { name: 'Rinkeby Test Token', symbol: 'TST', decimals: 18, address: '0xb7dDCF6B64C05D76Adc497AE78AD83ba3883A294' },
     { name: 'Decentraland - Chainbreakers', symbol: 'MANA', decimals: 18, address: '0x0f8528c53fecb54b7005525a3e797e261a51b88e' },
