60e0f428a39e958c3416888994e00c3e0a0bdcc5
Jigar Patel <jigar@arc10.io>
2022-04-21 13:45:58 -0400

    BNB Testnet

--- dist/tokens/custom/index.js
+++ dist/tokens/custom/index.js
@@ -1,10 +1,25 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
 exports.customTokens = {
-    canonicalWrappedEther: { name: 'Testnet Canonical Wrapped BNB', symbol: 'WETH', decimals: 18, address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd' },
-    otherTokens: [
-        { name: 'Rinkeby Test Token', symbol: 'TST', decimals: 18, address: '0xb7dDCF6B64C05D76Adc497AE78AD83ba3883A294' },
-        { name: 'Decentraland - Chainbreakers', symbol: 'MANA', decimals: 18, address: '0x0f8528c53fecb54b7005525a3e797e261a51b88e' },
-    ],
-};
+	canonicalWrappedEther: {
+		name: 'Testnet Canonical Wrapped BNB',
+		symbol: 'WBNB',
+		decimals: 18,
+		address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd'
+	},
+	otherTokens: [
+		{
+			name: 'Rinkeby Test Token',
+			symbol: 'TST',
+			decimals: 18,
+			address: '0xb7dDCF6B64C05D76Adc497AE78AD83ba3883A294'
+		},
+		{
+			name: 'Decentraland - Chainbreakers',
+			symbol: 'MANA',
+			decimals: 18,
+			address: '0x0f8528c53fecb54b7005525a3e797e261a51b88e'
+		}
+	]
+}
 //# sourceMappingURL=index.js.map
