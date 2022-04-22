86c535fdeaf8b18ac19e6f60e132afe042c82d4e
Jigar Patel <jigar@arc10.io>
2022-04-22 12:30:50 -0400

    Mainnet BSC Changes

--- dist/tokens/custom/index.js
+++ dist/tokens/custom/index.js
@@ -1,10 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.customTokens = {
-    canonicalWrappedEther: { name: 'Testnet Canonical Wrapped BNB', symbol: 'WBNB', decimals: 18, address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd' },
-    otherTokens: [
-        { name: 'Rinkeby Test Token', symbol: 'TST', decimals: 18, address: '0xb7dDCF6B64C05D76Adc497AE78AD83ba3883A294' },
-        { name: 'Decentraland - Chainbreakers', symbol: 'MANA', decimals: 18, address: '0x0f8528c53fecb54b7005525a3e797e261a51b88e' },
-    ],
+    canonicalWrappedEther: { name: 'Canonical Wrapped BNB', symbol: 'WBNB', decimals: 18, address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c' },
+    otherTokens: [],
 };
 //# sourceMappingURL=index.js.map
--- src/tokens/custom/index.ts
+++ src/tokens/custom/index.ts
@@ -3,9 +3,7 @@ import {
 } from '../../types';
 
 export const customTokens: NetworkTokens = {
-  canonicalWrappedEther: { name: 'Testnet Canonical Wrapped BNB', symbol: 'WBNB', decimals: 18, address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd' },
+  canonicalWrappedEther: { name: 'Canonical Wrapped BNB', symbol: 'WBNB', decimals: 18, address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c' },
   otherTokens: [
-    { name: 'Rinkeby Test Token', symbol: 'TST', decimals: 18, address: '0xb7dDCF6B64C05D76Adc497AE78AD83ba3883A294' },
-    { name: 'Decentraland - Chainbreakers', symbol: 'MANA', decimals: 18, address: '0x0f8528c53fecb54b7005525a3e797e261a51b88e' },
   ],
 };
